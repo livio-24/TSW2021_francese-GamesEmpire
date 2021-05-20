e3ef1fb65f6e988c55bd7c581b4e1c783489c369
livio-24 <vona.livio@gmail.com>
2021-05-20 15:39:34 +0200

    modifica db

--- GamesEmpireApp/db/storage.sql
+++ GamesEmpireApp/db/storage.sql
@@ -3,7 +3,7 @@ CREATE DATABASE storage;
 USE storage;
 
 CREATE TABLE metodo_pagamento( 
-numero_carta int primary key,
+numero_carta varchar(16) primary key,
 scadenza_carta date not null,
 titolare_carta varchar(100) not null
 );
@@ -15,13 +15,14 @@ cap varchar(100) ,
 provincia varchar(100) not null,
 nome varchar(100) not null,
 cognome varchar(100) not null,
-citt√† varchar(100) not null,
+citt‡ varchar(100) not null,
 primary key(indirizzo, cap) );
 
 CREATE TABLE prodotto (
 id_prodotto int primary key auto_increment,
 nome varchar(100) not null,
 descrizione varchar(100) not null,
+descrizione_dettagliata text,
 iva varchar(100) not null,
 in_vendita boolean not null,
 data_uscita date not null,
@@ -39,7 +40,7 @@ nome varchar(100) not null,
 cognome varchar(100) not null,
 data_nascita date not null,
 amministratore boolean not null,
-carta_credito int,
+carta_credito varchar(16),
 indirizzo varchar(100) ,
 cap varchar(100) ,
 foreign key (carta_credito) references metodo_pagamento (numero_carta)
@@ -52,11 +53,12 @@ CREATE TABLE ordine (
 id_ordine int primary key AUTO_INCREMENT,
 stato varchar(100) not null,
 data_ordine date not null,
-cliente varchar(100) not null,
-carta_credito int not null,
-indirizzo varchar(100) not null,
-cap varchar(100) not null ,
-foreign key (cliente) references cliente (email)
+email varchar(100) not null,
+importo_totale double not null,
+carta_credito varchar(16) ,
+indirizzo varchar(100) ,
+cap varchar(100) ,
+foreign key (email) references cliente (email)
 	on update cascade on delete no action,
 foreign key(carta_credito) references metodo_pagamento(numero_carta)
 	on update no action on delete no action,
@@ -91,4 +93,7 @@ foreign key(id_prodotto) references prodotto(id_prodotto)
 );
 
 insert into prodotto (nome,descrizione,prezzo,quantita,data_uscita,iva,in_vendita,immagine,piattaforma,genere) values
-	("FIFA 21", "FIFA 21 - PLAYSTATION 5", "35.99", "50", "15-10-2020", "3%", true,"C:\Users\Livio\Desktop\images", "PS5", "calcio");
+("Spider-Man Miles Morales", "Marvel's Spider-Man Miles Morales - PlayStation 4", "45.99", "60", "2020/12/13", "3%", true,"images\\SpiderManMilesMoralesPS4.jpg", "PlayStation 4", "Avventura"),
+("fifa 21", "Fifa 21 - PlayStation 5", "35.99", "50", "2020/0/13", "3%", true,"images\\fifa21PS5.jpg", "PlayStation 5", "calcio"),
+("Mario Kart 8 Deluxe", "Mario Kart 8 Deluxe - Switch", "51.00", "60", "2017/4/28", "3%", true,"images\\MarioKart8Deluxe.jpg", "Nintendo Switch", "Gioco Di Guida"),
+("Devil May Cry 5 - Special Edition", "Devil May Cry 5 (Special Edition) - Xbox Series", "41.00", "30", "2020/12/01", "3%", true,"images\\DevilMayCry5SpecialEditionXboxSeries.jpg", "Xbox Series", "Azione");
